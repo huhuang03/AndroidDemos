120cd0567d6fda04acd014774ad078a0149f5a8d
tonghu <huhuang03@gmail.com>
2014-08-27 14:10:36 +0800

    修改基础结构

---
+++ src/com/example/tonghu/apidemo/view/PopMenuDemo.java
@@ -0,0 +1,14 @@
+package com.example.tonghu.apidemo.view;
+
+import com.example.tonghu.apidemo.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PopMenuDemo extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
