622136d4b1fed4e95c2737dd4f38581a60c51a0d
tonghu <huhuang03@gmail.com>
2014-09-02 09:36:11 +0800

    增加clone animation

--- src/com/example/tonghu/apidemo/view/PopMenuDemo.java
+++ src/com/example/tonghu/apidemo/view/PopMenuDemo.java
@@ -1,14 +1,14 @@
 package com.example.tonghu.apidemo.view;
 
-import com.example.tonghu.apidemo.R;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.PopupMenu;
-import android.widget.Toast;
 import android.widget.PopupMenu.OnMenuItemClickListener;
+import android.widget.Toast;
+
+import com.example.tonghu.apidemo.R;
 
 public class PopMenuDemo extends Activity {
 	@Override
