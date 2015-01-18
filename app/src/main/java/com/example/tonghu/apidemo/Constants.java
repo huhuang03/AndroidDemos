ee495f775acff48275b920cee3236b84fb721955
tonghu <huhuang03@gmail.com>
2015-01-18 17:26:30 +0800

    update nothing

--- app/src/main/java/com/example/tonghu/apidemo/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.tonghu.apidemo;
-
-public class Constants {
-	public static final String CATEGORY = "com.example.tonghu.apiidemo.category.APIDEMO";
-	public static final String EXTRA_PATH_PREFIX = "extra_path";
-}
--- app/src/main/java/com/example/tonghu/apidemo/view/SwipeRefreshDemo.java
+++
@@ -1,8 +0,0 @@
-package com.example.tonghu.apidemo.view;
-
-/**
- * Created by tonghu on 1/14/15.
- */
-public class SwipeRefreshDemo {
-
-}
