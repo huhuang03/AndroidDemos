4818002c6426ab6ac7ae17d52a0641f29c816fb5
tonghu <huhuang03@gmail.com>
2015-01-18 23:12:52 +0800

    nothing

--- guaguaka/src/main/java/com/example/tonghu/guaguaka/view/SketchpadView.java
+++ guaguaka/src/main/java/com/example/tonghu/guaguaka/view/SketchpadView.java
@@ -13,7 +13,7 @@ import android.graphics.Xfermode;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
-import android.view.View
+import android.view.View;
 
 import com.example.tonghu.guaguaka.R;
 
